4d6fadcdf2cc996bf46cc0752e3a32f5e90d3f95
FiveYellowMice <hkz85825915@gmail.com>
2026-01-01 03:42:30 +1100

    Add license

--- lib.lua
+++ lib.lua
@@ -1,3 +1,8 @@
+-- (C) 2025 FiveYellowMice
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 local gui_inventory_slot = {}
 
 ---Options to adjust the behaviour of the slot.
